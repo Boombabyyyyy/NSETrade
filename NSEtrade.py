21bbeb3f39254bf17b08e0af4eb3a6b3f52cb09a
Azfar Faheem Mustafa <azfar.faheem@gmail.com>
2022-07-03 07:33:44 +0530

    Add files via upload

---
+++ NSEtrade.py
@@ -0,0 +1,29 @@
+# Imports
+from urllib.error import HTTPError, URLError
+from nsetools import Nse
+import time
+nse = Nse()
+
+# Function that fetches stock price
+def get_current_price(scrip):
+        q = nse.get_quote(scrip)['lastPrice']
+        print(q)
+
+# Driver code
+print("Please enter the unique stock scrip for the company you're looking to trade:")
+stock = input()
+while True:
+    try:
+        get_current_price(stock)
+        time.sleep(5)
+    except HTTPError:
+        print("Something went wrong, please try again later!")
+        break
+    except URLError:
+        print("Please check your internet connection and retry!")
+        break
+    except TypeError:
+        print("No such stock exists, please enter a valid Scrip and retry")
+        break
+
+
